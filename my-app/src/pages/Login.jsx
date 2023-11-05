f62d22a9ca966df086e3c2a84883747520007a4a
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-11-05 19:23:22 +0000

    login.jsx-started&home.jsx

---
+++ my-app/src/pages/Login.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const Login = () => {
+  
+    return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Kam Chat</span>
+        <span className="title">Register</span>
+        <form>
+          <input required type="email" placeholder="email" />
+          <input required type="password" placeholder="password" />  
+          <button>Sign in</button>
+        </form>
+        <p>Don't you have an account? Register</p>
+      </div>
+    </div>
+  )
+}
+
+export default Login;

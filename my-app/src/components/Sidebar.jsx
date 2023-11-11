f8322efcf27efbe4152f3460cdf9c992cd8a33e3
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-11-11 11:35:14 +0000

    cha-page-navbar-created

--- my-app/src/components/Sidebar.jsx
+++ my-app/src/components/Sidebar.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Navbar"
+import Search from "./Search"
+import Chats from "./Chats"
 
 const Sidebar = () => {
   return (
-    <div>
-     
+    <div className="sidebar">
+      <Navbar />
+      <Search/>
+      <Chats/>
     </div>
-  )
-}
+  );
+};
 
 export default Sidebar;

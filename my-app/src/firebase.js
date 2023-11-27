8c9ef6332b0bc4063ad4e19ea7d1b7fe8881cc2f
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-11-27 16:42:55 +0000

    firebase-added

---
+++ my-app/src/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDunBUAp4Qo84ZRpVdNbj25GZUHeKDJDrU",
+  authDomain: "chat-de44d.firebaseapp.com",
+  projectId: "chat-de44d",
+  storageBucket: "chat-de44d.appspot.com",
+  messagingSenderId: "531121272673",
+  appId: "1:531121272673:web:29358a5d3c3a287e553720"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth()

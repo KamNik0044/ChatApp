3a2e420abfc0e670ca1309e9aac7c67ea6649f2c
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-12-03 17:48:36 +0000

    database-created

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,6 +1,8 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 
 const firebaseConfig = {
@@ -14,4 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth()
+export const auth = getAuth()const storage = getStorage();
+export const storage = getStorage();
+export const db = getFirestore()

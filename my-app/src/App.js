0e03b940d1b68d4b874b40a0767566b18f6497f2
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-12-09 19:15:56 +0000

    authContext

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,23 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import "./style.scss"
 import Login from "./pages/Login";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom"
 
 function App() {
   return (
-      
-      <Register/>
-   
+      <BrowserRouter>
+       <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="Register" element={<Register />} />
+        </Route>
+       </Routes>
+      </BrowserRouter>
   );
 }
 
---
+++ my-app/src/context/AuthContext.js
@@ -0,0 +1,12 @@
+import { createContext, useEffect, useState } from "react";
+
+export const AuthContext = createContext()
+
+export const AuthContextProvider  = ({children}) =>{
+    const [currentUser,setCurrentUser] = useState({})
+
+    useEffect(()=>{
+
+    },[])
+    
+}

cf8b7ff91f1715a1af5e54e4af2f842fd14cf839
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2023-12-11 20:49:13 +0000

    updateSearch

--- my-app/src/firebase.js
+++ my-app/src/firebase.js
@@ -1,21 +1,19 @@
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
-
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDunBUAp4Qo84ZRpVdNbj25GZUHeKDJDrU",
-  authDomain: "chat-de44d.firebaseapp.com",
-  projectId: "chat-de44d",
-  storageBucket: "chat-de44d.appspot.com",
-  messagingSenderId: "531121272673",
-  appId: "1:531121272673:web:29358a5d3c3a287e553720"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "chat-ab746.firebaseapp.com",
+  projectId: "chat-ab746",
+  storageBucket: "chat-ab746.appspot.com",
+  messagingSenderId: "901216368405",
+  appId: "1:901216368405:web:8ec942ee51611df5c49b1c",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth()const storage = getStorage();
+export const auth = getAuth();
 export const storage = getStorage();
-export const db = getFirestore();
+export const db = getFirestore()
